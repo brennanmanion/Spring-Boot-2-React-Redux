09955ef91923ada5ffafc266ca21d2eddec3b040
Brennan Manion <brennanmanion@Brennans-MBP-2.home>
2022-02-19 17:04:40 -0700

    first commit

---
+++ src/main/java/com/example/demo/SpringBoot2ReactReduxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2ReactReduxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2ReactReduxApplication.class, args);
+	}
+
+}
